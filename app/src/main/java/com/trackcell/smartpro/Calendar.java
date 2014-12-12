33d992e6cfe616cf9bc79724ebae0567de5c35e9
Clint Mourlevat <clint.mourlevat@gmail.com>
2014-12-12 12:35:28 +0100

    adding missing files

---
+++ app/src/main/java/com/trackcell/smartpro/Calendar.java
@@ -0,0 +1,71 @@
+package com.trackcell.smartpro;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CalendarView;
+
+public class Calendar extends Activity
+{
+    String[] MONTHS = {"Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"};
+
+    private CalendarView mCalendar;
+    private int mYear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_calendar);
+
+        overridePendingTransition(R.anim.trans_left_in, R.anim.trans_left_out);
+
+        mCalendar = (CalendarView)findViewById(R.id.calendarView);
+
+        mCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
+        {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth)
+            {
+                setTitle(dayOfMonth + " " + MONTHS[month] + " " + year);
+            }
+        });
+    }
+
+    public void CloseActivity(MenuItem item)
+    {
+        setResult(RESULT_OK, new Intent().putExtra("result", 1));
+        finish();
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        setResult(RESULT_OK, new Intent().putExtra("result", 1));
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.calendar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                CloseActivity(null);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
